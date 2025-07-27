dd2800432b60928b4b2673e5aff9e654f88fdf15
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2025-07-27 22:03:06 +0200

    into gif added and its shown before the app opens

--- PostInfoPortal/app/_layout.tsx
+++ PostInfoPortal/app/_layout.tsx
@@ -1,10 +1,30 @@
-import {Stack} from "expo-router";
+import {enableScreens} from 'react-native-screens';
 
+enableScreens();
+
+import {SplashScreen as ExpoSplashScreen, Stack} from 'expo-router';
 import "./global.css";
 import {StatusBar} from "react-native";
 import {AuthProvider} from "@/components/AuthContext";
+import {useState} from "react";
+import SplashScreen from '@/components/SplashScreen';
 
 export default function RootLayout() {
+
+    const [showCustomSplash, setShowCustomSplash] = useState(true);
+
+    if (showCustomSplash) {
+        return (
+            <SplashScreen
+                onFinish={() => {
+                    setShowCustomSplash(false);
+                    ExpoSplashScreen.hideAsync();
+                }}
+            />
+        );
+    }
+
+
     return (
         <AuthProvider>
             <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent={true} animated={true}/>
---
+++ PostInfoPortal/components/SplashScreen.tsx
@@ -0,0 +1,44 @@
+import {useEffect, useRef} from 'react';
+import {View, Animated, Dimensions} from 'react-native';
+import {Image} from 'expo-image';
+import {StatusBar} from 'expo-status-bar';
+
+export default function SplashScreen({onFinish}: { onFinish: () => void }) {
+
+    const translateX = useRef(new Animated.Value(0)).current;
+
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            Animated.timing(translateX, {
+                toValue: -Dimensions.get('window').width,
+                duration: 500,
+                useNativeDriver: true,
+            }).start(() => {
+                onFinish();
+            });
+        }, 7000); // gif length
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <Animated.View
+            className="bg-white"
+            style={{
+                flex: 1,
+                transform: [{translateX}]
+
+            }}>
+
+            <StatusBar hidden/>
+            <Image
+                source={require('@/assets/animations/intro.gif')}
+                style={{width: '100%', height: '100%'}}
+                contentFit="contain"
+                contentPosition="center"
+            />
+
+        </Animated.View>
+    );
+}
