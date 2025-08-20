7cb2af99820fde84c98fafa01333b7c73ade4273
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2025-08-20 21:01:47 +0200

    _layout.tsx fonts update for ios

--- PostInfoPortal/app/_layout.tsx
+++ PostInfoPortal/app/_layout.tsx
@@ -1,59 +1,62 @@
 import { enableScreens } from "react-native-screens";
 import { SplashScreen as ExpoSplashScreen, Stack, useRouter } from "expo-router";
 import "./global.css";
-import { StatusBar } from "react-native";
+import {StatusBar } from "react-native";
 import React from "react";
 import SplashScreen from "@/components/SplashScreen";
 import { ThemeProvider } from "@/components/ThemeContext";
 import { useOneSignalDeepLinks } from "@/hooks/useOneSignalDeepLinks";
 import { RouteTarget } from "@/types/routeDeepLink";
-import { useFonts } from "expo-font";
+import {useFonts} from "expo-font";
 
 enableScreens();
-
-ExpoSplashScreen.preventAutoHideAsync().catch(() => {});
 
 export default function RootLayout() {
     const router = useRouter();
     const [showCustomSplash, setShowCustomSplash] = React.useState(true);
 
     const [fontsLoaded] = useFonts({
-        "Roboto-Bold": require("../assets/fonts/Roboto-Bold.ttf"),
-        "Roboto-ExtraBold": require("../assets/fonts/Roboto-ExtraBold.ttf"),
-        "Roboto-Light": require("../assets/fonts/Roboto-Light.ttf"),
-        "Roboto-Medium": require("../assets/fonts/Roboto-Medium.ttf"),
-        "Roboto-Regular": require("../assets/fonts/Roboto-Regular.ttf"),
-        "Roboto-SemiBold": require("../assets/fonts/Roboto-SemiBold.ttf"),
-        "YesevaOne-Regular": require("../assets/fonts/YesevaOne-Regular.ttf"),
+        'Roboto-Bold': require('../assets/fonts/Roboto-Bold.ttf'),
+        'Roboto-ExtraBold': require('../assets/fonts/Roboto-ExtraBold.ttf'),
+        'Roboto-Light': require('../assets/fonts/Roboto-Light.ttf'),
+        'Roboto-Medium': require('../assets/fonts/Roboto-Medium.ttf'),
+        'Roboto-Regular': require('../assets/fonts/Roboto-Regular.ttf'),
+        'Roboto-SemiBold': require('../assets/fonts/Roboto-SemiBold.ttf'),
+        'YesevaOne-Regular': require('../assets/fonts/YesevaOne-Regular.ttf'),
+
     });
 
-    React.useEffect(() => {
-        if (fontsLoaded && !showCustomSplash) {
-            ExpoSplashScreen.hideAsync().catch(() => {});
-        }
-    }, [fontsLoaded, showCustomSplash]);
-
+    // hides splash when openning the notification
     const fastRoute = (to: RouteTarget) => {
-        setShowCustomSplash(false); // ovo će trigger-ovati hide gore kad fontsLoaded === true
+        setShowCustomSplash(false);
+        try {
+            ExpoSplashScreen.hideAsync();
+        } catch {}
         requestAnimationFrame(() => router.push(to as any));
     };
 
+    // OneSignal + deep links (Android-only for now)
     useOneSignalDeepLinks({
         navigate: fastRoute,
         onesignalAppId: "9f87ae6e-83b5-4f3b-b49f-1f293096109d",
-        debug: true,
+        debug: true, // set false in production
     });
 
-    if (!fontsLoaded || showCustomSplash) {
+    const rootKey = fontsLoaded ? "fonts-ready" : "fonts-loading";
+
+    if (showCustomSplash) {
         return (
             <SplashScreen
-                onFinish={() => setShowCustomSplash(false)}
+                onFinish={() => {
+                    setShowCustomSplash(false);
+                    ExpoSplashScreen.hideAsync();
+                }}
             />
         );
     }
 
     return (
-        <ThemeProvider>
+        <ThemeProvider key={rootKey}>
             <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent animated />
             <Stack>
                 <Stack.Screen name="index" options={{ headerShown: false }} />
